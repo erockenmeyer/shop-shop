68c77b50538ffc7fbcf9c12a28d4f5aacc1dd021
erockenmeyer <erikrockenmeyer@gmail.com>
2022-06-30 16:19:38 -0500

    cart saves to idb & app can load offline

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -16,12 +16,27 @@ function ProductList() {
 
   useEffect(() => {
     if (data) {
+      // store in global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+      // also save to idb w helper
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      })
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
